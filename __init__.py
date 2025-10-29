a63b22f53a5c0a4113e82d7ed0ad8c4c8d05aa14
opYPUC <prodeadpoolpro@mail.ru>
2025-10-29 19:06:39 +0300

    понижение версии пайтона для блэктеста

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from .activity_cog import ActivityCog
+
 # ТЕСТ КОГА
+
 
 def setup(bot):
     bot.add_cog(ActivityCog(bot))
--- activity_cog.py
+++ activity_cog.py
@@ -8,11 +8,15 @@ class ActivityCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name='Roblox + '
-                                                                                                         'Minecraft + DND'))
+        await self.bot.change_presence(
+            activity=discord.Activity(
+                type=discord.ActivityType.playing, name="Roblox + " "Minecraft + DND"
+            )
+        )
 
     @commands.command()
     async def change_status(self, ctx: commands.Context, status: str):
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=status))
+        await self.bot.change_presence(
+            activity=discord.Activity(type=discord.ActivityType.playing, name=status)
+        )
         await ctx.send(f"Статус изменен на: `{status}`")
-
